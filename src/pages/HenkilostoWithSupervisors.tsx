1bf5ec7d87a09d023c3885eadf19163d8c047e32
jethronevalainen-lgtm <269593616+jethronevalainen-lgtm@users.noreply.github.com>
2026-07-27 14:04:06 +0300

    refactor: remove separate supervisor assignment panel

--- src/pages/HenkilostoWithSupervisors.tsx
+++ src/pages/HenkilostoWithSupervisors.tsx
@@ -1,11 +1,5 @@
-import EmployeeSupervisorAssignments from '@/components/EmployeeSupervisorAssignments';
 import Henkilosto from '@/pages/Henkilosto';
 
 export default function HenkilostoWithSupervisors() {
-  return (
-    <div className="space-y-6">
-      <EmployeeSupervisorAssignments />
-      <Henkilosto />
-    </div>
-  );
+  return <Henkilosto />;
 }
