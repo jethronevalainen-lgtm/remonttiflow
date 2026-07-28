af54f0913c87a7a08357bc1a27dbbe443f9af4a1
jethronevalainen-lgtm <269593616+jethronevalainen-lgtm@users.noreply.github.com>
2026-07-28 09:16:35 +0300

    Reititä tilaajan uusi työpyyntösivu

--- src/App.tsx
+++ src/App.tsx
@@ -43,6 +43,7 @@ const Hallinta = lazy(() => import('./pages/HallintaV2'));
 const KayttajaEsikatselu = lazy(() => import('./pages/KayttajaEsikatseluV2'));
 const Tilaukset = lazy(() => import('./pages/Tilaukset'));
 const TilaajanTyot = lazy(() => import('./pages/TilaajanTyotV2'));
+const NewCustomerWorkRequest = lazy(() => import('./pages/NewCustomerWorkRequest'));
 const CustomerProject = lazy(() => import('./pages/CustomerProjectV2'));
 const CustomerCollaborationManager = lazy(() => import('./pages/CustomerCollaborationManager'));
 const ProjectDiscussions = lazy(() => import('./pages/ProjectDiscussions'));
@@ -168,6 +169,7 @@ function AppRoutes() {
         <Route path="/tyomaaraykset" element={<RoleGuard allowedRoles={rolesForRoute('/tyomaaraykset')}><Tyomaaraykset /></RoleGuard>} />
         <Route path="/tilaukset" element={<RoleGuard allowedRoles={rolesForRoute('/tilaukset')}><Tilaukset /></RoleGuard>} />
         <Route path="/tilaajan-tyot" element={<RoleGuard allowedRoles={rolesForRoute('/tilaajan-tyot')}><TilaajanTyot /></RoleGuard>} />
+        <Route path="/tilaajan-tyot/uusi" element={<RoleGuard allowedRoles={rolesForRoute('/tilaajan-tyot')}><NewCustomerWorkRequest /></RoleGuard>} />
         <Route path="/tilaajan-projektit/:projectId" element={<RoleGuard allowedRoles={rolesForRoute('/tilaajan-projektit')}><CustomerProject /></RoleGuard>} />
         <Route path="/tyovuorokalenteri" element={<RoleGuard allowedRoles={rolesForRoute('/tyovuorokalenteri')}><Tyovuorokalenteri /></RoleGuard>} />
         <Route path="/tuntikirjaukset" element={<RoleGuard allowedRoles={rolesForRoute('/tuntikirjaukset')}><TimeEntriesRoute /></RoleGuard>} />
